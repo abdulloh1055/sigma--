d55b7139ade4d400d12300f9b42c3978220dcd03
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-05-15 11:31:26 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,11 @@
+let ism = prompt("ismingizni kriting");
+let id = prompt("spase idgizni kriting");
+let ps = prompt("posporttingizni kriting");
+
+let data ={
+    ism: ism,
+    id: id,
+    ps: ps,
+}
+
+console.log(data);
